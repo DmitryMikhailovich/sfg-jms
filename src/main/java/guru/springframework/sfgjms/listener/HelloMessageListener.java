0c795a6a9ab736dde580f41e3f2fb72298f1fd89
dmitry <dmitry.mm@icloud.com>
2021-09-30 23:42:07 +0300

    add

---
+++ src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgjms.listener;
+
+import guru.springframework.sfgjms.config.JMSConfig;
+import guru.springframework.sfgjms.model.HelloWorldMessage;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Message;
+
+@Component
+public class HelloMessageListener {
+
+    @JmsListener(destination = JMSConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage hwMessage,
+                       @Headers MessageHeaders headers, Message message) {
+
+        System.out.println("I've got a message!");
+
+        System.out.println(hwMessage.getMessage());
+    }
+}
